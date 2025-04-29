43d69e1cb5f6dd075b9bfdc5391837df4a4f5b6d
jose.hernandezm <jose.hernandezm@enginecore.com.mx>
2025-04-29 18:24:10 +0000

    [FIX] document annotation module

--- backend/src/documents/document-annotation.module.ts
+++ backend/src/documents/document-annotation.module.ts
@@ -1,21 +1,17 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentAnnotationService } from './document-annotation.service';
 import { DocumentAnnotationController } from './document-annotation.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentAnnotation } from './entities/document-annotation.entity';
 import { Document } from './entities/document.entity';
 import { DocumentsService } from './documents.service';
-import { CryptoService } from 'src/crypto/crypto.service';
-import { AuditLogService } from 'src/audit/audit-log.service';
-import { SharingService } from 'src/sharing/sharing.service';
-import { BlockchainService } from 'src/blockchain/blockchain.service';
-import { DocumentProcessorService } from './processors/document-processor.service';
-import { DocumentAnalyzerService } from 'src/analyzers/document-analyzer.service';
+import { CryptoModule } from 'src/crypto/crypto.module';
+import { AuditModule } from 'src/audit/audit.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DocumentAnnotation, Document])],
+  imports: [TypeOrmModule.forFeature([DocumentAnnotation, Document]), CryptoModule, AuditModule],
+  providers: [DocumentAnnotationService, DocumentsService],
   controllers: [DocumentAnnotationController],
-  providers: [DocumentAnnotationService, DocumentsService, CryptoService, AuditLogService, SharingService, BlockchainService, DocumentProcessorService, DocumentAnalyzerService],
   exports: [DocumentAnnotationService]
 })
 export class DocumentAnnotationModule {}

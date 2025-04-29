b2df09664006a4581c085a52e6c1d60879a0650c
Jose Manuel Hernandez Martinez <jose.hernandezm@enginecore.com.mx>
2025-04-29 12:59:05 -0600

    [FIX] api bug

--- backend/src/documents/document-annotation.module.ts
+++ backend/src/documents/document-annotation.module.ts
@@ -7,12 +7,18 @@ import { Document } from './entities/document.entity';
 import { DocumentsService } from './documents.service';
 import { CryptoModule } from 'src/crypto/crypto.module';
 import { AuditModule } from 'src/audit/audit.module';
-import { SharingModule } from '../sharing/sharing.module'; // Import SharingModule
-
+import { SharingModule } from '../sharing/sharing.module';
+import { BlockchainModule } from '../blockchain/blockchain.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([DocumentAnnotation, Document]), CryptoModule, AuditModule, SharingModule], // Add SharingModule to imports
+  imports: [
+    TypeOrmModule.forFeature([DocumentAnnotation, Document]),
+    CryptoModule,
+    AuditModule,
+    SharingModule,
+    BlockchainModule, // Añadir el módulo aquí
+  ],
   providers: [DocumentAnnotationService, DocumentsService],
   controllers: [DocumentAnnotationController],
-  exports: [DocumentAnnotationService]
+  exports: [DocumentAnnotationService],
 })
 export class DocumentAnnotationModule {}
